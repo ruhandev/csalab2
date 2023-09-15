77682ddd566a236e1eb15371f038fc11deb2fa53
Ruhananhad P. Reddy <67246773+ruhandev@users.noreply.github.com>
2023-09-15 09:49:21 -0500

    Add files via upload

---
+++ Input2.java
@@ -0,0 +1,31 @@
+
+//NAME - 
+//DATE - 
+//PURPOSE - 
+
+import java.util.Scanner;
+
+public class Input2
+{
+    
+	public static void main (String[] args)
+	{
+	    Scanner keyboard = new Scanner(System.in);
+	  
+		System.out.print("Enter an integer :: ");
+		intOne = keyboard.nextInt();
+
+
+		System.out.print("Enter an integer :: ");
+		intTwo = keyboard.nextInt();
+		
+		System.out.print("Enter an integer to multiply by ::");
+		multBy = keyboard.nextInt();
+		
+		System.out.print("Enter your favorite number ::");
+		favNum = keyboard.nextString();
+
+
+		
+	}
+}
